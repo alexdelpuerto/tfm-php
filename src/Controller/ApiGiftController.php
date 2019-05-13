08cbf29416df4f30cd972b2cb4eef21a961232c0
Alex <alejandro.delpuerto2@gmail.com>
2019-05-13 14:31:05 +0200

    Controlador post Gifts issue #10

--- src/Controller/ApiGiftController.php
+++ src/Controller/ApiGiftController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
-
 use App\Entity\Event;
+use App\Entity\Gift;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,12 +36,52 @@ class ApiGiftController extends AbstractController {
                 Response::HTTP_OK);
     }
 
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route(path="", name="post", methods={"POST"})
+     */
+    public function postGifts(Request $request): Response {
+        $dataRequest = $request->getContent();
+        $data = json_decode($dataRequest, true);
+
+        $em = $this->getDoctrine()->getManager();
+        $event = $em->getRepository(Event::class)->findOneBy(array('id' => $data['eventId']));
+
+        if($event===null){
+            return $this->error400();
+        }else {
+            $gift = new Gift(
+                $data['name'],
+                $data['description'],
+                $data['price'],
+                $event
+            );
+
+            $em->persist($gift);
+            $em->flush();
+
+            return new JsonResponse(
+                ['gift' => $gift],
+                Response::HTTP_CREATED
+            );
+        }
+    }
+
     private function error404(): JsonResponse{
         $message = [
             'code' => Response::HTTP_NOT_FOUND,
             'message' => "No hay regalos para el evento buscado"
         ];
         return new JsonResponse($message, Response::HTTP_NOT_FOUND);
+    }
+
+    private function error400(): JsonResponse{
+        $message = [
+            'code' => Response::HTTP_BAD_REQUEST,
+            'message' => "Elige un evento"
+        ];
+        return new JsonResponse($message, Response::HTTP_BAD_REQUEST);
     }
 
 }
