1c7d0668359d3404e776888f426190f1b82471ea
Alex <alejandro.delpuerto2@gmail.com>
2019-05-16 11:47:44 +0200

    Definir la estructura de datos payments issue #11

---
+++ src/Entity/Payment.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Payment
+ *
+ * @ORM\Table(name="payment", indexes={@ORM\Index(name="buyer", columns={"buyer", "person"})})
+ * @ORM\Entity
+ */
+class Payment implements \JsonSerializable
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="buyer", type="string", length=20, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    private $buyer;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="person", type="string", length=20, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    private $person;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $price;
+
+    /**
+     * Payment constructor.
+     * @param string $buyer
+     * @param string $person
+     * @param float $price
+     */
+    public function __construct(string $buyer, string $person, float $price)
+    {
+        $this->buyer = $buyer;
+        $this->person = $person;
+        $this->price = $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBuyer(): string
+    {
+        return $this->buyer;
+    }
+
+    /**
+     * @param string $buyer
+     */
+    public function setBuyer(string $buyer): void
+    {
+        $this->buyer = $buyer;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPerson(): string
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param string $person
+     */
+    public function setPerson(string $person): void
+    {
+        $this->person = $person;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function jsonSerialize()
+    {
+        return array(
+            'buyer'         => $this->buyer,
+            'person'        => $this->person,
+            'price'         => $this->price
+        );
+    }
+}
