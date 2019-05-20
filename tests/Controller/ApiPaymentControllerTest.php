a2090b80782798c34fada8831bd3badf36cd7321
Alex <alejandro.delpuerto2@gmail.com>
2019-05-20 16:43:34 +0200

    Test Controlador get pagos deben issue #14

--- tests/Controller/ApiPaymentControllerTest.php
+++ tests/Controller/ApiPaymentControllerTest.php
@@ -14,8 +14,13 @@ class ApiPaymentControllerTest extends WebTestCase{
     /**@var Client $client*/
     private static $client;
 
+    public static $username;
+    public static $usernameError;
+
     public static function setUpBeforeClass(){
         self::$client = static::createClient();
+        self::$username = 'user7';
+        self::$usernameError = 'user75';
     }
 
     /**
@@ -31,5 +36,35 @@ class ApiPaymentControllerTest extends WebTestCase{
         self::$client->request(Request::METHOD_POST, ApiPaymentController::PAYMENT_API_PATH,
             [], [], [], json_encode($data));
         self::assertEquals(Response::HTTP_CREATED, self::$client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Implements testGetCollections
+     * @covers ::getCollections
+     */
+    public function testGetCollections(): void {
+        self::$client->request(Request::METHOD_GET, ApiPaymentController::PAYMENT_API_PATH . '/' . self::$username);
+        $body = self::$client->getResponse()->getContent();
+        self::assertJson($body);
+
+        $data = json_decode($body, true);
+        self::assertArrayHasKey('collections', $data);
+        self::assertEquals(Response::HTTP_OK, self::$client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Implements testGetCollectionsError
+     * @covers ::getCollections
+     */
+    public function testGetCollectionsError(): void {
+        self::$client->request(Request::METHOD_GET, ApiPaymentController::PAYMENT_API_PATH . '/' . self::$usernameError);
+        self::assertEquals(Response::HTTP_NOT_FOUND, self::$client->getResponse()->getStatusCode());
+        $body = self::$client->getResponse()->getContent();
+        self::assertJson($body);
+
+        $message = json_decode($body, true);
+        self::assertArrayHasKey('code', $message);
+        self::assertArrayHasKey('message', $message);
+        self::assertEquals(Response::HTTP_NOT_FOUND, $message['code']);
     }
 }
